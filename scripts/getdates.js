c19473c0167fa2a44dafcecc7938bbf6c609393e
kelvin-melfah <kmelfah@byupathway.edu>
2026-05-08 21:40:35 +0000

    2nd commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically populate the copyright year
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// Dynamically populate the last modified date
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
